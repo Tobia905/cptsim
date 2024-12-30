0f564672f4c1c34ff2408e95df32256ad646b0c8
Tobia905 <tobiatommasini@gmail.com>
2024-12-31 00:20:14 +0100

    minor changes

--- cptsim/agent.py
+++ cptsim/agent.py
@@ -4,8 +4,6 @@ from cptsim.tax import progressive_tax
 
 class EconomicAgent:
 
-    saving_rate = .20
-
     def __init__(
         self, 
         min_tax: float = 0.15, 
@@ -13,6 +11,7 @@ class EconomicAgent:
         min_inc: int | float = 500, 
         max_inc: int | float = 7500, 
         prog_rate: float = 0.0007,
+        saving_rate: float = .20,
         constant_tax: bool = False
     ):
         self.income = simulate_income(n=1)[0]
@@ -22,7 +21,7 @@ class EconomicAgent:
             )
             if not constant_tax else .22
         )
-        self.available_income = self.income * (1 - self.saving_rate)
+        self.available_income = self.income * (1 - saving_rate)
 
     def buy_consumption_good(self, price: float):
         self.available_income -= price + (self.tax * price)
